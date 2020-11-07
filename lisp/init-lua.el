1d72c7b4c3b1bc47fba29712c519d59ba77cd67a
josedvm95 <josedvm95@gmail.com>
2020-11-07 21:37:31 +0100

    Put Lua config in language specific file.

---
+++ lisp/init-lua.el
@@ -0,0 +1,19 @@
+;;; init-lua.el --- Lua editing -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+
+;; Enable and install Lua mode automatically (for highlighting)
+(use-package lua-mode
+  :ensure t
+  :mode (("\\.lua$" . lua-mode))
+  :custom
+  (lua-indent-level 2)
+  (indent-tabs-mode nil))
+
+;; Use love-minor-mode-v2 for the LÖVE framework
+(add-to-list 'load-path (expand-file-name "love-minor-mode-v2" user-emacs-directory))
+(use-package love-minor-mode-v2)
+
+(provide 'init-lua)
+;;; init-lua.el ends here
