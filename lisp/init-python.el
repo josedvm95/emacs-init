8d4d5e942cc393d7f459e30e5ab4ece087153ce5
josedvm95 <josedvm95@gmail.com>
2020-11-08 01:22:26 +0100

    Improve Python editing with anaconda mode.

---
+++ lisp/init-python.el
@@ -0,0 +1,24 @@
+;;; init-python.el --- Python editing -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+
+(setq python-shell-interpreter "python3")
+
+;; Enable anaconda for code navegation, documentation lookup and code completion.
+(use-package anaconda-mode
+  :ensure t
+  :after (python)
+  :diminish anaconda-mode
+  :hook ((python-mode . anaconda-mode)
+         (python-mode . anaconda-eldoc-mode)))
+
+;; For company mode with anaconda
+;(use-package company-anaconda
+;  :ensure t
+;  :after (anaconda-mode company)
+;  :config
+;  (add-to-list 'company-backends 'company-anaconda))
+
+(provide 'init-python)
+;;; init-python.el ends here
